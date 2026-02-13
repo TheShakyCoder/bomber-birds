edc1898f78679a4bd840838be415f501a31fdfc0
Sharif Khan <>
2026-02-13 13:59:20 +0000

    build config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -9,11 +9,13 @@ module.exports = {
   apps : [{
     name: "colyseus-app",
     script: 'build/index.js',
+    interpreter: 'bun',
     time: true,
     watch: false,
     instances: os.cpus().length,
     exec_mode: 'fork',
     wait_ready: true,
+    max_memory_restart: '2G',
   }],
 };
 
