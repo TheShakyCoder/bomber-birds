c2d19db77dfd3d5586c747ef56aa13e2bc16a109
Sharif Khan <>
2026-02-13 14:08:08 +0000

    build error

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,18 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    sourcemap: false,
+    cssCodeSplit: true,
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+      },
+    },
+    rollupOptions: {
+      maxParallelFileOps: 2,
+      cache: false,
+    }
+  }
 })
