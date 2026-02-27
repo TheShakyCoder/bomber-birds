a2dea6788e2687b7addac05ad7d70d569f7fa829
Sharif Khan <>
2026-02-27 17:53:15 +0000

    bun pm2

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
             // TODO: Switch to instances: "max" + exec_mode: "cluster" once Redis is ready
             instances: "max",
             exec_mode: "cluster",
-            interpreter: "node",
+            interpreter: "bun",
             env_production: {
                 NODE_ENV: "production",
                 REDIS_URL: "redis://" + process.env.REDIS_URL,  // Uncomment when Redis is ready
