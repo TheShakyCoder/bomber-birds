4539fd8c2a793226d40d54d89fab84b3094f756b
Sharif Khan <>
2026-02-27 20:54:20 +0000

    env

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
         {
-            name: "sbm",
+            name: "bomber-birds",
             script: "build/index.js",
             // TODO: Switch to instances: "max" + exec_mode: "cluster" once Redis is ready
             instances: 1,
@@ -10,6 +10,11 @@ module.exports = {
             env_production: {
                 NODE_ENV: "production",
                 REDIS_URL: "redis://10.0.0.6:6379",
+                VITE_SERVER_PORT: 2567,
+                VITE_DOMAIN: "bomber-birds.on-forge.com",
+                VITE_SERVER_URL: "https://bomber-birds.on-forge.com/colyseus/",
+                VITE_WS_URL: "wss://bomber-birds.on-forge.com/colyseus/",
+                VITE_ORIGIN: "https://bomber-birds.on-forge.com",
             },
         },
     ],
