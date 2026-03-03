f1eb310564c54a8d9d0a2718ce27661182b08241
Sharif Khan <>
2026-03-03 22:08:50 +0000

    eco

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,16 +6,7 @@ module.exports = {
             // TODO: Switch to instances: "max" + exec_mode: "cluster" once Redis is ready
             instances: 1,
             exec_mode: "fork",
-            interpreter: "node",
-            env_production: {
-                NODE_ENV: "production",
-                VITE_SERVER_PORT: 2567,
-                VITE_DOMAIN: "bomber-birds.sharifkhan.co.uk",
-                VITE_SERVER_URL: "https://bomber-birds.sharifkhan.co.uk/colyseus/",
-                VITE_WS_URL: "wss://bomber-birds.sharifkhan.co.uk/colyseus/",
-                VITE_ORIGIN: "https://bomber-birds.sharifkhan.co.uk",
-                COLYSEUS_PUBLIC_ADDRESS: "bomber-birds.sharifkhan.co.uk/colyseus",
-            },
+            interpreter: "node"
         },
     ],
 };
